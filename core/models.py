9793312c0c44f36f139921a1d99ade7478bbb425
Arquedy <samuel812767@hotmail.com>
2020-02-01 12:26:04 -0300

    ciado tabela de dados par eventos para trabalhar com a agenda

--- core/models.py
+++ core/models.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now = True)
+    usuario = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+
+    class Meta:
+        pass
+    def __str__(self):
+            return self.titulo
